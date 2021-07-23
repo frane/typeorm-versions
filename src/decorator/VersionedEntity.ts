add071cdb1abac4a5b9f354d79731bad4af396a3
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2021-07-23 16:17:44 +0200

    Add support of ManyToMany relation

--- src/decorator/VersionedEntity.ts
+++ src/decorator/VersionedEntity.ts
@@ -6,6 +6,11 @@ export function VersionedEntity() {
     return Reflect.metadata(versionedEntityMetadataKey, true);
 }
 
-export function isVersionedEntity(target: Object) {
-    return Reflect.getMetadata(versionedEntityMetadataKey, target.constructor) === true;
+// Target is null when interacting with a manytomany relation
+export function isVersionedEntity(target?: Object) {
+  if (!target) {
+    return false;
+  }
+
+  return Reflect.getMetadata(versionedEntityMetadataKey, target.constructor) === true;
 }
